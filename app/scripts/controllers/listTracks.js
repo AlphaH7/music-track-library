5b05d9c8607efcc61be00ac63e40579d274b1b44
AlphaH <Alistier@Admins-MacBook-Air.local>
2017-05-19 21:28:21 +0530

    list tracks and events genre complete

--- app/scripts/controllers/listTracks.js
+++ app/scripts/controllers/listTracks.js
@@ -1,10 +1,10 @@
-angular.module('track-lib').controller('list',function($scope , $resource , env , $http){
+angular.module('track-lib').controller('list',function($scope , trackService , $resource , env , $http){
   $scope.displayMode = "list";
   $scope.tracks = null;
 
   $scope.list = function (arg) {
       $scope.arg = arg;
-      $scope.allTracks = $resource(env + arg);
+      $scope.allTracks = $resource(env + arg + ":query" , { query: "@query" });
       $scope.tracks = $scope.allTracks.get();
       console.log($scope.tracks)
   };
@@ -14,6 +14,8 @@ angular.module('track-lib').controller('list',function($scope , $resource , env 
       $scope.tracks = $scope.nextLink.get();
       console.log($scope.tracks)
   };
+
+  $scope.search
 
 
 });
