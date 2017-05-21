c49c9456d4d3af091b99401273d35feb41c291a1
AlphaH <Alistier@Admins-MacBook-Air.local>
2017-05-21 17:09:14 +0530

    next and prev page for tracks done

--- app/scripts/controllers/listTracks.js
+++ app/scripts/controllers/listTracks.js
@@ -3,6 +3,8 @@ angular.module('track-lib').controller('list',function($scope ,trackService , $r
   $scope.getTracks = null;
   $scope.readOnly = true;
   $scope.clearResults = false;
+  $scope.currentPage = 1;
+  $scope.pages=[];
 
   $scope.addrating = function(rating){
     $scope.s.rating = rating;
@@ -26,6 +28,7 @@ angular.module('track-lib').controller('list',function($scope ,trackService , $r
       console.log($scope.tracks);
       $scope.clearResults = false;
       $scope.addBtn = true;
+      $scope.editBtn = true;
       $scope.search = true;
     });
   };
@@ -34,6 +37,8 @@ angular.module('track-lib').controller('list',function($scope ,trackService , $r
 
   $scope.nextPage = function (d) {
       $scope.nextLink = $resource(d);
+      $scope.pages.push(d)
+      $scope.currentPage++ ;
       $scope.getTracks = $scope.nextLink.get(
         function(){
           $scope.tracks = $scope.getTracks.results;
@@ -42,6 +47,23 @@ angular.module('track-lib').controller('list',function($scope ,trackService , $r
         }
       );
       console.log($scope.tracks)
+      console.log($scope.pages)
+  };
+
+  $scope.prevPage = function () {
+      $scope.prev = $scope.pages[$scope.pages.length - $scope.currentPage]
+      console.log($scope.currentPage)
+      $scope.currentPage-- ;
+      $scope.nextLink = $resource($scope.prev);
+      $scope.getTracks = $scope.nextLink.get(
+        function(){
+          $scope.tracks = $scope.getTracks.results;
+          console.log($scope.tracks);
+          $scope.clearResults = false;
+        }
+      );
+      console.log($scope.tracks)
+      console.log($scope.prev)
   };
 
   $scope.postTrack = function (d) {
@@ -61,17 +83,25 @@ angular.module('track-lib').controller('list',function($scope ,trackService , $r
       angular.extend(x, d);
       x.$get(function(response) {
       console.log('response:', response);
-      if(response.$resolved){
-        $scope.tracks = [response];
-        $scope.clearResults = true;
-      }else{
+      if(response.detail == "Not found."){
+        console.log('nf')
         $mdToast.show(
           $mdToast.simple()
-            .textContent('Something went wrong. Plz try again.')
+            .textContent("Track not found.")
             .position('top right')
             .hideDelay(3000)
         );
-      }
+      }else  if(response.$resolved){
+        $scope.tracks = [response];
+        $scope.clearResults = true;
+      }else {
+        $mdToast.show(
+          $mdToast.simple()
+            .textContent("Something went wrong. Plz check and try again.")
+            .position('top right')
+            .hideDelay(3000)
+        );
+      };
       });
     }
   };
