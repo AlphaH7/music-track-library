13626e865aab8e38790db2ed31e21fa555edf656
AlphaH <alistier.hea@gmail.com>
2017-05-24 13:21:42 +0530

    added refresh button for mobile

--- app/scripts/directives/effects.js
+++ app/scripts/directives/effects.js
@@ -6,8 +6,6 @@ angular.module('track-lib').directive('effect', function($window) {
 
     restrict: 'AE',
     link: function(s, e, a) {
-      var body = angular.element(document.getElementsByTagName("body"));
-
 
       $window.onresize = function(event) {
         var clientWidth = document.documentElement.clientWidth;
